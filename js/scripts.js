c396e46732613db5ce452e327e06a5a369082295
KrystinaGerke <83786005+KrystinaGerke@users.noreply.github.com>
2021-05-30 18:00:48 -0400

    changed the spacing and code grammar

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,22 @@
 let pokemonList = [
-  { name = Pikachu
-  type = electric
-  height = .4
-}
-{ name = Bulbasaur
-type = grass/poison
-height= .7
-}
-{ name = Charmander
-  type = fire
-  height = .6
-}
-{ name = Squirtle
-type = water
-height = .5
+{
+  name: "Pikachu",
+  type: [electric],
+  height: 0.4
+},
+{
+  name: "Bulbasaur",
+  type: [grass, poison],
+  height: 0.7
+},
+{
+  name: "Charmander",
+  type: [fire],
+  height: 0.6
+},
+{
+  name:  "Squirtle",
+  type: [water],
+  height: 0.5
 }
 ]
